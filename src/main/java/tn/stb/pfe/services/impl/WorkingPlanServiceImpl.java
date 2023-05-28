446370fc0e24a8d9d9b58cad6aae75a537d17d39
Eya024 <ayachakroun@gmail.com>
2023-05-28 20:10:30 +0100

    updates

--- src/main/java/tn/stb/pfe/services/impl/WorkingPlanServiceImpl.java
+++ src/main/java/tn/stb/pfe/services/impl/WorkingPlanServiceImpl.java
@@ -1,14 +1,21 @@
 package tn.stb.pfe.services.impl;
 
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import tn.stb.pfe.models.Appointment;
 import tn.stb.pfe.models.DayPlan;
 import tn.stb.pfe.models.TimePeroid;
 import tn.stb.pfe.models.WorkingPlan;
 import tn.stb.pfe.models.user.provider.Provider;
+import tn.stb.pfe.repositories.AppointmentRepository;
 import tn.stb.pfe.repositories.WorkingPlanRepository;
 import tn.stb.pfe.repositories.user.UserRepository;
 import tn.stb.pfe.services.WorkingPlanService;
@@ -19,10 +26,12 @@ public class WorkingPlanServiceImpl implements WorkingPlanService {
 
     private final WorkingPlanRepository workingPlanRepository;
     private final UserRepository providerRepository;
+    private final AppointmentRepository appointmentRepository;
 
-    public WorkingPlanServiceImpl(WorkingPlanRepository workingPlanRepository, UserRepository providRepository) {
+    public WorkingPlanServiceImpl(WorkingPlanRepository workingPlanRepository, UserRepository providRepository, AppointmentRepository appointmentRepository) {
         this.workingPlanRepository = workingPlanRepository;
         this.providerRepository = providRepository;
+        this.appointmentRepository = appointmentRepository;
     }
 
 
@@ -57,6 +66,9 @@ public class WorkingPlanServiceImpl implements WorkingPlanService {
         workingPlan.setFriday(fridayPlan);
         workingPlan.setSaturday(saturdayPlan);
         workingPlan.setSunday(sundayPlan);
+
+        // Generate appointments based on the working plan
+          List<Appointment> appointments = generateAppointments(workingPlan);
 
         // Save the working plan to the database using the workingPlanRepository
         workingPlanRepository.save(workingPlan);
@@ -99,4 +111,74 @@ public class WorkingPlanServiceImpl implements WorkingPlanService {
     }
 
 
+
+    private List<Appointment> generateAppointments(WorkingPlan workingPlan) {
+        List<Appointment> appointments = new ArrayList<>();
+    
+        // Get the working hours for each day of the week
+        TimePeroid mondayWorkingHours = workingPlan.getMonday().getWorkingHours();
+        TimePeroid tuesdayWorkingHours = workingPlan.getTuesday().getWorkingHours();
+        TimePeroid wednesdayWorkingHours = workingPlan.getWednesday().getWorkingHours();
+        TimePeroid thursdayWorkingHours = workingPlan.getThursday().getWorkingHours();
+        TimePeroid fridayWorkingHours = workingPlan.getFriday().getWorkingHours();
+        TimePeroid saturdayWorkingHours = workingPlan.getSaturday().getWorkingHours();
+        TimePeroid sundayWorkingHours = workingPlan.getSunday().getWorkingHours();
+
+        // Get the breaks for each day of the week
+        List<TimePeroid> mondayBreaks = workingPlan.getMonday().getBreaks();
+        List<TimePeroid> tuesdayBreaks = workingPlan.getTuesday().getBreaks();
+        List<TimePeroid> wednesdayBreaks = workingPlan.getWednesday().getBreaks();
+        List<TimePeroid> thursdayBreaks = workingPlan.getThursday().getBreaks();
+        List<TimePeroid> fridayBreaks = workingPlan.getFriday().getBreaks();
+        List<TimePeroid> saturdayBreaks = workingPlan.getSaturday().getBreaks();
+        List<TimePeroid> sundayBreaks = workingPlan.getSunday().getBreaks();
+
+        // Generate the appointments for each day of the week
+        appointments.addAll(generateAppointmentsForDay(mondayWorkingHours, mondayBreaks, DayOfWeek.MONDAY));
+        appointments.addAll(generateAppointmentsForDay(tuesdayWorkingHours, tuesdayBreaks, DayOfWeek.TUESDAY));
+        appointments.addAll(generateAppointmentsForDay(wednesdayWorkingHours, wednesdayBreaks, DayOfWeek.WEDNESDAY));
+        appointments.addAll(generateAppointmentsForDay(thursdayWorkingHours, thursdayBreaks, DayOfWeek.THURSDAY));
+        appointments.addAll(generateAppointmentsForDay(fridayWorkingHours, fridayBreaks, DayOfWeek.FRIDAY));
+        appointments.addAll(generateAppointmentsForDay(saturdayWorkingHours, saturdayBreaks, DayOfWeek.SATURDAY));
+        appointments.addAll(generateAppointmentsForDay(sundayWorkingHours, sundayBreaks, DayOfWeek.SUNDAY));
+
+        return appointments;
+    }
+
+    // generateAppointmentsForDay
+    private List<Appointment> generateAppointmentsForDay(TimePeroid workingHours, List<TimePeroid> breaks, DayOfWeek dayOfWeek) {
+        List<Appointment> appointments = new ArrayList<>();
+        LocalTime startTime = workingHours.getStart();
+        LocalTime endTime = workingHours.getEnd();
+
+        // Generate the appointments for the day
+        while (startTime.isBefore(endTime)) {
+            // Check if the current time is in a break
+            if (!isInBreak(startTime, breaks)) {
+                // Create a new appointment
+                Appointment appointment = new Appointment();
+
+                // convert localtime to localdatetime
+                LocalDateTime startDateTime = LocalDateTime.of(LocalDate.now(), startTime);
+                appointment.setStart(startDateTime);
+                appointments.add(appointment);
+                appointmentRepository.save(appointment);
+            }
+            startTime = startTime.plusMinutes(30);
+        }
+        return appointments;
+    }
+
+
+
+    private boolean isInBreak(LocalTime startTime, List<TimePeroid> breaks) {
+        for (TimePeroid breakTime : breaks) {
+            if (startTime.isAfter(breakTime.getStart()) && startTime.isBefore(breakTime.getEnd())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
 }
